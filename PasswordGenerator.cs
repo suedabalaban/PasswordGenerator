471242d4080497b12c01eb426f868fcf2d7e040f
USER <USER@LAPTOP-8KFBDQNU>
2023-03-17 12:53:19 +0300

    Add project files.

---
+++ PasswordGenerator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    public class PasswordGenerator
+    {
+        StringBuilder builder = new StringBuilder();
+        Random random = new Random();
+        char ch;
+
+        public string Password(int length, int upperCase, int lowerCase, int specialChar, int number)
+        {
+            if (length == upperCase + lowerCase + specialChar + number)
+            {
+                StringBuilder bd = new StringBuilder();
+                bd.Append(LowerString(lowerCase));
+                bd.Append(SpecialChar(specialChar));
+                bd.Append(UpperString(upperCase));
+                bd.Append(RandomNumber(number));
+            }
+            else
+            {
+                Console.WriteLine("Length must be equal to the sum of the characters.");
+            }
+            return builder.ToString();
+        }
+        public string UpperString(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                //returns a random uppercase letter in accordance with ASCII reference.
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            return builder.ToString();
+        }
+        public string LowerString(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                //returns a random lowercase letter in accordance with ASCII reference.
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 97)));
+                builder.Append(ch);
+            }
+            return builder.ToString();
+        }
+        public string SpecialChar(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                //returns a random special character in accordance with ASCII reference.
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(14 * random.NextDouble() + 33)));
+                builder.Append(ch);
+            }
+            return builder.ToString();
+        }
+        public string RandomNumber(int size)
+        {
+            int num;
+            for (int i = 0; i < size; i++)
+            {
+                //returns a random integer between 0 and 10.
+                num = random.Next(0, 10);
+                builder.Append(num);
+            }
+            return builder.ToString();
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PasswordGenerator generator = new PasswordGenerator();
+
+            Console.WriteLine("-----Password Generator-----");
+
+            //User input to determine the features of password.
+
+            Console.Write("Determine the length of the password: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Determine how many uppercase letter to be created: ");
+            int upperCase = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Determine how many lowercase letter to be created: ");
+            int lowerCase = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Determine how many special character to be created: ");
+            int specialChar = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Determine how many number to be created: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Random Password is : {0} ", generator.Password(length, upperCase, lowerCase, specialChar, number));
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
+
